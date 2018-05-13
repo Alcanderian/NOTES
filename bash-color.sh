38677e62719e1de347e37fd02b295f2610ffba20
Alcanderian <Alcanderian@users.noreply.github.com>
2018-05-14 01:36:54 +0800

    Create bash-color.sh

---
+++ bash-color.sh
@@ -0,0 +1,8 @@
+#enables colorin the terminal bash shell export
+export CLICOLOR=1
+
+#sets up thecolor scheme for list export
+export LSCOLORS=gxfxcxdxbxegedabagacad
+
+#sets up the prompt color (currently a green similar to linux terminal)
+export PS1="\[\033[01;31m\][\[\033[01;33m\]\u@\h\[\033[00m\] \[\033[01;37m\]\w\[\033[00m\]\[\033[01;31m\]]\[\e[0m\]\$ "
