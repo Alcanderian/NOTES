dfa508bc3333b1b21f15231a0ab7402fb3699ff9
Alcanderian <Alcanderian@users.noreply.github.com>
2020-12-03 13:46:02 +0800

    Update bash-color.sh

--- tools/bash-color.sh
+++ tools/bash-color.sh
@@ -5,4 +5,5 @@ export CLICOLOR=1
 export LSCOLORS=gxfxcxdxbxegedabagacad
 
 #sets up the prompt color (currently a green similar to linux terminal)
-export PS1="\[\033[01;31m\][\[\033[01;33m\]\u@\h\[\033[00m\] \[\033[01;37m\]\w\[\033[00m\]\[\033[01;31m\]]\[\e[0m\]\$ "
+MYIP=`ifconfig eth0 |grep "inet addr"| cut -f 2 -d ":"|cut -f 1 -d " "`
+export PS1="\[\033[01;33m\]\u\[\033[01;31m\]@\[\033[01;33m\]$MYIP\[\033[00m\]:\[\033[01;37m\]\w\[\033[01;31m\]\$\[\e[0m\] "
